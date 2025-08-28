d85967a88b6d6306fdcfed3e632c1e071730c4fa
jerosas00567 <jeronimosalazar770@gmail.com>
2025-08-28 17:37:32 -0500

    .

---
+++ 03-tipo-numb.js
@@ -0,0 +1,17 @@
+let num1 = "7";
+let num2 = "5";
+
+let result = num1 +num2;
+console.log(result);
+
+// En JS todos los numeros tienen el mismo tratamiento, a diferencia de otros lenfuajes que exigen su tipado
+
+let num3 = 7;
+let num4 = 5;
+
+let result2 = num3 + num4;
+
+console.log(result2);
+
+let userName = "jero";
+console.log(userName + num3);
---
+++ 04-objeto-math.js
@@ -0,0 +1,23 @@
+let result;
+
+result = Math.PI;
+result = Math.round(2.7);
+result = Math.round(2.5);
+
+result = Math.ceil(2.1); //redondeo hacia arriba 
+result = Math.floor(2.8);// redondeo hacia abajo
+result = Math.sqrt(16);//Raiz cuadrada
+result = Math.abs(-144);//Valor absoluto 
+result = Math.min(1, 5, 85, 65, 7);
+result = Math.max(1, 5, 85, 65, 7);
+result = Math.random();
+result = Math.floor(math.random() * 8);//Imprimir numeros enteros
+
+console.log(result);
+
+
+
+
+
+
+
